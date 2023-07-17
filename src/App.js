a4912712ec9354e45224acbe924ff5a8cda14167
Musa SAYAR <musasayar67@gmail.com>
2023-07-17 12:39:49 +0300

    helmet

--- src/App.js
+++ src/App.js
@@ -4,26 +4,38 @@ import Language from "./Compentents/Language";
 import ResultsTyping from "./Compentents/ResultsTyping";
 import TypingArea from "./Compentents/TypingArea";
 import WordArea from "./Compentents/WordArea";
-
+import { Helmet } from "react-helmet"
 
 function App() {
-  const { isModal} = useSelector((state)=>state.typingSpeed)
+  const { isModal } = useSelector((state) => state.typingSpeed)
 
 
   return (
-    <div >
-      <Header/>
-      <div className=" container flex  flex-col items-center justify-center "> 
-      <Language/>
-      <WordArea/>
-      <TypingArea/>
-      {
-        isModal &&   <ResultsTyping/>
-      }
-   
+
+    <>
+      <Helmet>
+
+        <title>Typing Speed App</title>
+        <meta name="description" content="typing speed description" />
+        <link rel="shortcut icon" href="/typing-app/public/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/typing-app/public/favicon.ico" type="image/x-icon"></link>
+      </Helmet>
+
+      <div >
+        <Header />
+        <div className=" container flex  flex-col items-center justify-center ">
+          <Language />
+          <WordArea />
+          <TypingArea />
+          {
+            isModal && <ResultsTyping />
+          }
+
+        </div>
+
       </div>
-   
-    </div>
+    </>
+
   );
 }
 
